6f1f2bee88310523efc5c56125734d5e15f949f5
Chanaka Abeysinghe <chanakasan@gmail.com>
2023-06-28 11:43:07 +0530

    add userService test

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 // import './tests/sum.test.js';
 // import './tests/movies.test.js';
-import './tests/users.test.js';
+// import './tests/users.test.js';
+import './tests/user-service.test.js';
 
 mocha.checkLeaks();
 mocha.run();
---
+++ user-service.js
@@ -0,0 +1,69 @@
+async function request(method, url, data=null) {
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+  const options = {
+    headers,
+    method,
+  }
+  if (data) {
+    options.body = JSON.stringify(data)
+  }
+  const res = await fetch(url, options)
+  return res
+}
+
+async function getUsers() {
+  const res = await request('GET', '/users') 
+  const body = await res.json()
+  return body.users
+}
+
+async function getUser(id) {
+  if (!id) {
+    throw new Error('You must provide an id');
+  }
+  const res = await request('GET', `/users/${id}`) 
+  const body = await res.json()
+  return body.user
+}
+
+async function createUser(attrs) {
+  const params = {
+    user: attrs
+  }
+  const res = await request('POST', `/users`, params) 
+  const body = await res.json()
+  return body.user
+}
+
+async function updateUser(id, attrs) {
+  if (!id) {
+    throw new Error('You must provide an id');
+  }
+  const params = {
+    user: attrs
+  }
+  const res = await request('PATCH', `/users/${id}`, params) 
+  const body = await res.json()
+  return body.user
+}
+
+async function deleteUser(id) {
+  if (!id) {
+    throw new Error('You must provide an id');
+  }
+  const res = await request('DELETE', `/users/${id}`)
+  if (res.status === 204) {
+    return true
+  }
+  return false
+}
+
+export default {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+}
